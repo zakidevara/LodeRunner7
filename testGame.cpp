01989d2566cb5f8cc8a335e040d5c7e910725225
zakidevara <mzakidevara@gmail.com>
2019-03-14 14:11:08 +0700

    update proses penggambaran matriks

--- testGame.cpp
+++ testGame.cpp
@@ -19,6 +19,18 @@ typedef struct{
     int bot;
 }game;
 
+void drawRope(int x1,int y1,int x2, int y2){ //menggambar tangga
+    readimagefile("line.jpg",x1,y1,x2,y2);
+}
+
+void drawCoin(int x1,int y1,int x2, int y2){ //menggambar tangga
+    readimagefile("coin.jpg",x1,y1,x2,y2);
+}
+
+void drawExit(int x1,int y1,int x2, int y2){ //menggambar tangga
+    readimagefile("exit.jpg",x1,y1,x2,y2);
+}
+
 void drawLadder(int x1,int y1,int x2, int y2){ //menggambar tangga
     readimagefile("ladder.gif",x1,y1,x2,y2);
 }
@@ -69,20 +81,26 @@ void level1(game arr[BARIS][KOLOM],int* barisPlayer, int* kolomPlayer){
     arr[8][11].stage = 1;
     arr[8][10].stage = 1;
     arr[8][9].stage = 1;
+    arr[7][8].stage = 3;
+    arr[7][7].stage = 3;
+    arr[7][6].stage = 3;
+    arr[7][9].stage = 4;
+    arr[7][10].stage = 5;
+
 
     *barisPlayer = BARIS-2; *kolomPlayer = KOLOM/2;
     insertPlayer(arr,*barisPlayer, *kolomPlayer);
     insertBot(arr,BARIS-2,KOLOM-2);
 }
 
-void generateStage(game arr[BARIS][KOLOM], int level, int* barisPlayer, int* kolomPlayer){ //memasukkan nilai matriks game
+void generateStage(game arr[BARIS][KOLOM], int level, int* barisPlayer, int* kolomPlayer){
     switch(level){
     case 1 :
         level1(arr, barisPlayer, kolomPlayer);
     }
 }
 
-void displayStage(game arr[BARIS][KOLOM]){ //menampilkan matriks game
+void drawStage(game arr[BARIS][KOLOM]){ // menggambar seluruh matriks game
     for(int i=0; i<BARIS; i++){
         for(int j=0; j<KOLOM; j++){
             //penggambaran stage
@@ -90,6 +108,12 @@ void displayStage(game arr[BARIS][KOLOM]){ //menampilkan matriks game
                 drawBlock(MATRIX_ELEMENT_SIZE*j,MATRIX_ELEMENT_SIZE*i,MATRIX_ELEMENT_SIZE*(j+1),MATRIX_ELEMENT_SIZE*(i+1));
             }else if(arr[i][j].stage == 2){
                 drawLadder(MATRIX_ELEMENT_SIZE*j,MATRIX_ELEMENT_SIZE*i,MATRIX_ELEMENT_SIZE*(j+1),MATRIX_ELEMENT_SIZE*(i+1));
+            }else if(arr[i][j].stage == 3){
+                drawRope(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(i+1));
+            }else if(arr[i][j].stage == 4){
+                drawCoin(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(i+1));
+            }else if(arr[i][j].stage == 5){
+                drawExit(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(i+1));
             }
 
             //penggambaran player
@@ -105,8 +129,63 @@ void displayStage(game arr[BARIS][KOLOM]){ //menampilkan matriks game
     }
 }
 
+void drawStageBaris(game arr[BARIS][KOLOM], int baris){
 
-/*coord getPlayerCoord(game arr[BARIS][KOLOM]){
+    for(int j = 0; j < KOLOM; j++){
+        //penggambaran stage
+        if(arr[baris][j].stage == 1){
+            drawBlock(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*baris, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(baris+1));
+        }else if(arr[baris][j].stage == 2){
+            drawLadder(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*baris, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(baris+1));
+        }else if(arr[baris][j].stage == 3){
+            drawRope(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*baris, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(baris+1));
+        }else if(arr[baris][j].stage == 4){
+            drawCoin(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*baris, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(baris+1));
+        }else if(arr[baris][j].stage == 5){
+            drawExit(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*baris, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(baris+1));
+        }
+
+        //penggambaran player
+        if(arr[baris][j].player == 1){
+            drawSonic(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*baris, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(baris+1));
+        }
+
+        //penggambaran bot
+        if(arr[baris][j].bot == 1){
+            drawKnuckles(MATRIX_ELEMENT_SIZE*j, MATRIX_ELEMENT_SIZE*baris, MATRIX_ELEMENT_SIZE*(j+1), MATRIX_ELEMENT_SIZE*(baris+1));
+        }
+    }
+}
+
+void drawStageKolom(game arr[BARIS][KOLOM], int kolom){
+
+    for(int i = 0; i < BARIS; i++){
+        //penggambaran stage
+        if(arr[i][kolom].stage == 1){
+            drawBlock(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(i+1));
+        }else if(arr[i][kolom].stage == 2){
+            drawLadder(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(i+1));
+        }else if(arr[i][kolom].stage == 3){
+            drawRope(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(i+1));
+        }else if(arr[i][kolom].stage == 4){
+            drawCoin(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(i+1));
+        }else if(arr[i][kolom].stage == 5){
+            drawExit(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(i+1));
+        }
+
+        //penggambaran player
+        if(arr[i][kolom].player == 1){
+            drawSonic(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(i+1));
+        }
+
+        //penggambaran bot
+        if(arr[i][kolom].bot == 1){
+            drawKnuckles(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*i, MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(i+1));
+        }
+    }
+}
+
+coord getPlayerCoord(game arr[BARIS][KOLOM]){
     coord player;
     for(int i = 0; i < BARIS; i++){
         for(int j = 0; j < KOLOM; j++){
@@ -117,28 +196,24 @@ void displayStage(game arr[BARIS][KOLOM]){ //menampilkan matriks game
             }
         }
     }
-}*/
+}
 
 int main(){
-    initwindow(800, 650, " ", 0, 0, true, true);
+    initwindow(800, 650, " ", 0, 0, false, true);
     setaspectratio(4,3);
     char movement;
-    int page=0;
+    int page = 0;
     game arr[BARIS][KOLOM];
     int barisPlayer, kolomPlayer; //posisi player di matriks
 
 
     generateStage(arr, 1, &barisPlayer, &kolomPlayer);
-
+    drawStage(arr);
     while(true){
-        displayStage(arr);
+        //setactivepage(page);
+        //setvisualpage(1-page);
 
-        setactivepage(page);
-        setvisualpage(1-page);
-
-        clearviewport();
-
-        movement=getch();
+        movement = getch();
         switch(movement){
             case 'w' :
                 if(barisPlayer > 0){
@@ -166,9 +241,26 @@ int main(){
                     }
                     break;
         }
-        //setviewport(0,barisPlayer*MATRIX_ELEMENT_SIZE, 800,(barisPlayer+1)*MATRIX_ELEMENT_SIZE,1);
+
+
         insertPlayer(arr,barisPlayer,kolomPlayer);
-        page=1-page;
+
+        if(movement == 'a' || movement == 'd'){
+            setviewport(0,barisPlayer*MATRIX_ELEMENT_SIZE, 800,(barisPlayer+1)*MATRIX_ELEMENT_SIZE,0);
+            clearviewport();
+            setviewport(0,0, 800,600,1);
+            drawStageBaris(arr, barisPlayer);
+        }
+
+        if(movement == 'w' || movement == 's'){
+            setviewport(kolomPlayer*MATRIX_ELEMENT_SIZE,0, (kolomPlayer+1)*MATRIX_ELEMENT_SIZE,600,0);
+            clearviewport();
+            setviewport(0,0, 800,600,1);
+            drawStageKolom(arr, kolomPlayer);
+        }
+        movement = NULL;
+        page = 1-page;
+
     }
 
     getch();
