bf2722e1ef363e8c2e5ea8c5835da52899743807
an1sal <48077693+an1sal@users.noreply.github.com>
2019-03-21 14:26:46 +0700

    penambahan menu

--- 181511037.h
+++ 181511037.h
@@ -111,3 +111,50 @@ void draw2left(game arr[BARIS][KOLOM], int kolom, int baris){ //menggambar matri
         }
     }
 }
+
+
+void menutama()
+{
+    initwindow(getmaxwidth(), getmaxheight(), " ", 0, 0,false,true);
+    int playy = (600/2)+100;
+    int credity = playy+70;
+    int exity = credity+70;
+    int mousex,mousey;
+    char pil;
+
+
+    while(!ismouseclick(WM_LBUTTONDOWN)){
+
+    settextstyle(BOLD_FONT,HORIZ_DIR,7);
+    outtextxy(800/2,600/2,"LODE RUNNER");
+    settextstyle(EUROPEAN_FONT,HORIZ_DIR,4);
+    outtextxy(1100/2,playy,"PLAY");
+    settextstyle(EUROPEAN_FONT,HORIZ_DIR,4);
+    outtextxy(1050/2,credity,"CREDIT");
+    settextstyle(EUROPEAN_FONT,HORIZ_DIR,4);
+    outtextxy(1100/2,exity,"EXIT");
+
+
+    }
+    while(1){
+    getmouseclick(WM_LBUTTONDOWN, mousex, mousey);
+    //menu play
+    if ((mousex > 540) && (mousex < 675)&& (mousey > 390) && (mousey < 440)){
+        exit(1);
+    //menu credit
+        }else if ((mousex > 515) && (mousex < 695)&& (mousey > 460) && (mousey < 510)){
+            exit(1);
+    //menu exit
+                }else if ((mousex > 540) && (mousex < 659)&& (mousey > 530) && (mousey < 580)){
+                    cleardevice();
+                    settextstyle(COMPLEX_FONT,HORIZ_DIR,10);
+                    outtextxy(350,300,"GOOD BYE");
+                    delay(1000);
+                    exit(1);
+                }
+    }
+    printf("%d %d", mousex, mousey);
+    getch();
+}
+
+
