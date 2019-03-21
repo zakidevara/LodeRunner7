4507cbcc1f741d0d68c12864fba72e27819fe5bf
agi26 <48076471+agi26@users.noreply.github.com>
2019-03-21 11:21:18 +0700

    Update testGame.cpp

--- testGame.cpp
+++ testGame.cpp
@@ -408,8 +408,15 @@ bool isStanding(game arr[BARIS][KOLOM], int baris, int kolom){
     }
 }
 
-bool isClimbing(game arr[BARIS][KOLOM], int baris, int kolom){
-    if((arr[baris][kolom].stage == 2)||(arr[baris+1][kolom].stage == 2)){
+bool isClimbingUP(game arr[BARIS][KOLOM], int baris, int kolom){
+    if(((arr[baris][kolom].stage == 2)&&(arr[baris-1][kolom].stage == 2))||((arr[baris][kolom].stage == 2)&&(arr[baris-1][kolom].stage == 0))){
+        return true;
+    }else{
+        return false;
+    }
+}
+bool isClimbingDOWN(game arr[BARIS][KOLOM], int baris, int kolom){
+   if(((arr[baris+1][kolom].stage == 2)&&(arr[baris+1][kolom].stage == 2))||((arr[baris+1][kolom].stage == 2)&&(arr[baris][kolom].stage == 0))){
         return true;
     }else{
         return false;
@@ -430,6 +437,7 @@ bool isFalling(game arr[BARIS][KOLOM], int baris, int kolom){
     return false;}
 }
 
+
 bool isNabrak(game arr[BARIS][KOLOM], int baris, int kolom, int arah){// arah = -1 kalau ke kiri dan arah = 1 kalau ke kanan
     if((arr[baris][kolom+arah].stage == 1)){
         return true;
@@ -448,16 +456,18 @@ bool done(game arr[BARIS][KOLOM], int baris, int kolom)
 	}
 }
 
+
+
 void playerMovement(char movement, game arr[BARIS][KOLOM], int* barisPlayer, int* kolomPlayer){ //memindahkan posisi player dalam matriks sesuai movement yang dipilih oleh user
     switch(movement){
             case 'W' :
-                if((*barisPlayer > 0)&&(isClimbing(arr, *barisPlayer, *kolomPlayer)==true)|| (isStanding(arr, *barisPlayer, *kolomPlayer)==false)){
+                if((*barisPlayer > 0)&&(isClimbingUP(arr, *barisPlayer, *kolomPlayer)==true)|| (isStanding(arr, *barisPlayer, *kolomPlayer)==false)){
                     deletePlayer(arr,*barisPlayer,*kolomPlayer);
                     (*barisPlayer)--;
                     }
                     break;
             case 'S' :
-                if((*barisPlayer < BARIS-2) && (isStanding(arr, *barisPlayer, *kolomPlayer)==false)||(isClimbing(arr, *barisPlayer, *kolomPlayer)==true)||(isSliding(arr, *barisPlayer, *kolomPlayer)==true)){
+                if((*barisPlayer < BARIS-2) && (isStanding(arr, *barisPlayer, *kolomPlayer)==false)||(isClimbingDOWN(arr, *barisPlayer, *kolomPlayer)==true)||(isSliding(arr, *barisPlayer, *kolomPlayer)==true)){
                     deletePlayer(arr,*barisPlayer,*kolomPlayer);
                     (*barisPlayer)++;
                     }
