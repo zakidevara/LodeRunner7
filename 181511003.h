22594623f48c4c0133749fcfb2f2383552114651
agi26 <48076471+agi26@users.noreply.github.com>
2019-03-28 07:56:34 +0700

    Update 181511003.h

--- 181511003.h
+++ 181511003.h
@@ -2,16 +2,24 @@
 #include <graphics.h>
 #include <conio.h>
 
+// DEKLARASI MODUL
 bool isStanding(game arr[BARIS][KOLOM], int baris, int kolom){
-    if((arr[baris+1][kolom].stage == 1) || (arr[baris+1][kolom].stage == 2)){
+    if((arr[baris+1][kolom].stage == 1) || ((arr[baris+1][kolom].stage == 2)&&((arr[baris+1][kolom+1].stage == 1)||(arr[baris+1][kolom-1].stage == 1)))){
         return true;
     }else{
         return false;
     }
 }
 
-bool isClimbing(game arr[BARIS][KOLOM], int baris, int kolom){
-    if((arr[baris][kolom].stage == 2)){
+bool isClimbingUP(game arr[BARIS][KOLOM], int baris, int kolom){
+    if(((arr[baris][kolom].stage == 2)&&(arr[baris-1][kolom].stage == 2))||((arr[baris][kolom].stage == 2)&&(arr[baris-1][kolom].stage == 0))){
+        return true;
+    }else{
+        return false;
+    }
+}
+bool isClimbingDOWN(game arr[BARIS][KOLOM], int baris, int kolom){
+   if(((arr[baris+1][kolom].stage == 2)&&(arr[baris+1][kolom].stage == 2))||((arr[baris+1][kolom].stage == 2)&&(arr[baris][kolom].stage == 0))){
         return true;
     }else{
         return false;
@@ -26,52 +34,39 @@ bool isSliding(game arr[BARIS][KOLOM], int baris, int kolom){
     }
 }
 bool isFalling(game arr[BARIS][KOLOM], int baris, int kolom){
-    if((arr[baris+1][kolom].stage == 0) || (arr[baris+1][kolom].stage == 3)){
+    if(arr[baris+1][kolom].stage == 0){
         return true;
     }else{
-        return false;
-    }
+    return false;}
 }
 
-void playerMovement(char movement, game arr[BARIS][KOLOM], int* barisPlayer, int* kolomPlayer, int *X, int* Y){ //memindahkan posisi player dalam matriks sesuai movement yang dipilih oleh user
+void playerMovement(char movement, game arr[BARIS][KOLOM], int* barisPlayer, int* kolomPlayer){ //memindahkan posisi player dalam matriks sesuai movement yang dipilih oleh user
     switch(movement){
             case 'W' :
-            if((*Y-5 >= 0) && (isClimbing(arr, *barisPlayer, *kolomPlayer)) || ((arr[*barisPlayer+1][*kolomPlayer].stage == 2))){
-                *X = (*kolomPlayer)*MATRIX_ELEMENT_SIZE;
-                *Y= *Y - 5;
-            }
-            break;
-
+                if((*barisPlayer > 0)&&(isClimbingUP(arr, *barisPlayer, *kolomPlayer)==true)|| (isStanding(arr, *barisPlayer, *kolomPlayer)==false)){
+                    deletePlayer(arr,*barisPlayer,*kolomPlayer);
+                    (*barisPlayer)--;
+                    }
+                    break;
             case 'S' :
-            if((*Y+5 < WINDOWS_HEIGHT-MATRIX_ELEMENT_SIZE)  && ((isClimbing(arr, *barisPlayer, *kolomPlayer) && !isStanding(arr, *barisPlayer, *kolomPlayer))|| (isSliding(arr, *barisPlayer, *kolomPlayer) && !isStanding(arr, *barisPlayer, *kolomPlayer)) || isClimbing(arr, *barisPlayer+1, *kolomPlayer) || isFalling(arr, *barisPlayer, *kolomPlayer))){
-                if(isSliding(arr,*barisPlayer,*kolomPlayer)){
-                    *Y = *Y + 25;
-                }else{
-
-                    *X = (*kolomPlayer)*MATRIX_ELEMENT_SIZE;
-
-                    *Y = *Y + 5;
-                }
-            }
-            break;
+                if((*barisPlayer < BARIS-2) && (isStanding(arr, *barisPlayer, *kolomPlayer)==false)||(isClimbingDOWN(arr, *barisPlayer, *kolomPlayer)==true)||(isSliding(arr, *barisPlayer, *kolomPlayer)==true)){
+                    deletePlayer(arr,*barisPlayer,*kolomPlayer);
+                    (*barisPlayer)++;
+                    }
+                    break;
 
             case 'D' :
-            if((*X+5 < WINDOWS_WIDTH-MATRIX_ELEMENT_SIZE) && !isNabrak(arr, *X, *Y, 1) && ((isStanding(arr, *barisPlayer, *kolomPlayer)==true)||(isSliding(arr, *barisPlayer, *kolomPlayer)==true) || isClimbing(arr, *barisPlayer, *kolomPlayer))){
-                if(isStanding(arr, *barisPlayer, *kolomPlayer) && !isClimbing(arr, *barisPlayer, *kolomPlayer)){
-                    *Y = (*barisPlayer)*MATRIX_ELEMENT_SIZE;
-                }
-
-                *X = *X + 5;
-            }
-            break;
+                if((*kolomPlayer < KOLOM-2) && (isStanding(arr, *barisPlayer, *kolomPlayer)==true)||(isSliding(arr, *barisPlayer, *kolomPlayer)==true)){
+                    deletePlayer(arr,*barisPlayer,*kolomPlayer);
+                    (*kolomPlayer)++;
+                    }
+                    break;
             case 'A' :
-            if((*X-5 >=0) && !isNabrak(arr, *X, *Y, -1) && ((isStanding(arr, *barisPlayer, *kolomPlayer)==true)||(isSliding(arr, *barisPlayer, *kolomPlayer)==true) || isClimbing(arr, *barisPlayer, *kolomPlayer))){
-                if(isStanding(arr, *barisPlayer, *kolomPlayer) && !isClimbing(arr, *barisPlayer, *kolomPlayer)){
-                    *Y = (*barisPlayer)*MATRIX_ELEMENT_SIZE;
-                }
-                *X = *X - 5;
-            }
-            break;
+                if((*kolomPlayer > 1) && (isStanding(arr, *barisPlayer, *kolomPlayer)==true)||(isSliding(arr, *barisPlayer, *kolomPlayer)==true)){
+                    deletePlayer(arr,*barisPlayer,*kolomPlayer);
+                    (*kolomPlayer)--;
+                    }
+                    break;
         }
+        insertPlayer(arr,*barisPlayer,*kolomPlayer);
 }
-
