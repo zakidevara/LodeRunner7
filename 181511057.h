ac3d8594f4e834a7f4e5493f1778618ae85166a4
zakidevara <mzakidevara@gmail.com>
2019-03-28 07:50:35 +0700

    modul animasi lari

--- 181511057.h
+++ 181511057.h
@@ -1,24 +1,151 @@
+/* File         : 181511057.h               */
+/* Nama Pembuat : Muhammad Zaki Devara      */
+/* NIM          : 181511057                 */
+
 #include <stdio.h>
 #include <graphics.h>
 #include <conio.h>
-// DEKLARASI MODUL
-void insertBot(game arr[BARIS][KOLOM],int baris, int kolom){ // memasukkan bot dalam matriks game
+
+void drawPlayerLeft(int x1,int y1,int x2, int y2){
+// menggambar player menghadap kiri
+    readimagefile("images/player/Player_Left0.gif", x1, y1, x2-1, y2-1);
+}
+void drawPlayerRight(int x1,int y1,int x2, int y2){
+// menggambar player( menghadap kanan
+    readimagefile("images/player/Player_Right0.gif", x1, y1, x2-1, y2-1);
+}
+
+void playerRunningRight(int x1,int y1,int x2, int y2, int* urutan){
+    switch(*urutan){
+    case 0:
+        readimagefile("images/player/Player_Right0.gif", x1, y1, x2-1, y2-1);break;
+    case 1:
+        readimagefile("images/player/Player_Right1.gif", x1, y1, x2-1, y2-1);break;
+    case 2:
+        readimagefile("images/player/Player_Right2.gif", x1, y1, x2-1, y2-1);break;
+    case 3:
+        readimagefile("images/player/Player_Right3.gif", x1, y1, x2-1, y2-1);break;
+    case 4:
+        readimagefile("images/player/Player_Right4.gif", x1, y1, x2-1, y2-1);break;
+    case 5:
+        readimagefile("images/player/Player_Right5.gif", x1, y1, x2-1, y2-1);break;
+    case 6:
+        readimagefile("images/player/Player_Right6.gif", x1, y1, x2-1, y2-1);break;
+    case 7:
+        readimagefile("images/player/Player_Right7.gif", x1, y1, x2-1, y2-1);break;
+    }
+    (*urutan)++;
+    if(*urutan > 7){
+        *urutan = 0;
+    }
+
+}
+
+void playerRunningLeft(int x1,int y1,int x2, int y2, int* urutan){
+    switch(*urutan){
+    case 0:
+        readimagefile("images/player/Player_Left0.gif", x1, y1, x2-1, y2-1);break;
+    case 1:
+        readimagefile("images/player/Player_Left1.gif", x1, y1, x2-1, y2-1);break;
+    case 2:
+        readimagefile("images/player/Player_Left2.gif", x1, y1, x2-1, y2-1);break;
+    case 3:
+        readimagefile("images/player/Player_Left3.gif", x1, y1, x2-1, y2-1);break;
+    case 4:
+        readimagefile("images/player/Player_Left4.gif", x1, y1, x2-1, y2-1);break;
+    case 5:
+        readimagefile("images/player/Player_Left5.gif", x1, y1, x2-1, y2-1);break;
+    case 6:
+        readimagefile("images/player/Player_Left6.gif", x1, y1, x2-1, y2-1);break;
+    case 7:
+        readimagefile("images/player/Player_Left7.gif", x1, y1, x2-1, y2-1);break;
+    }
+    (*urutan)++;
+    if(*urutan > 7){
+        *urutan = 0;
+    }
+
+}
+
+void botRunningRight(int x1,int y1,int x2, int y2, int* urutan){
+    switch(*urutan){
+    case 0:
+        readimagefile("images/bot/Bot_Right0.gif", x1, y1, x2-1, y2-1);break;
+    case 1:
+        readimagefile("images/bot/Bot_Right1.gif", x1, y1, x2-1, y2-1);break;
+    case 2:
+        readimagefile("images/bot/Bot_Right2.gif", x1, y1, x2-1, y2-1);break;
+    case 3:
+        readimagefile("images/bot/Bot_Right3.gif", x1, y1, x2-1, y2-1);break;
+    case 4:
+        readimagefile("images/bot/Bot_Right4.gif", x1, y1, x2-1, y2-1);break;
+    case 5:
+        readimagefile("images/bot/Bot_Right5.gif", x1, y1, x2-1, y2-1);break;
+    case 6:
+        readimagefile("images/bot/Bot_Right6.gif", x1, y1, x2-1, y2-1);break;
+    case 7:
+        readimagefile("images/bot/Bot_Right7.gif", x1, y1, x2-1, y2-1);break;
+    }
+    (*urutan)++;
+    if(*urutan > 7){
+        *urutan = 0;
+    }
+
+}
+
+void botRunningLeft(int x1,int y1,int x2, int y2, int* urutan){
+    switch(*urutan){
+    case 0:
+        readimagefile("images/Bot/Bot_Left0.gif", x1, y1, x2-1, y2-1);break;
+    case 1:
+        readimagefile("images/Bot/Bot_Left1.gif", x1, y1, x2-1, y2-1);break;
+    case 2:
+        readimagefile("images/Bot/Bot_Left2.gif", x1, y1, x2-1, y2-1);break;
+    case 3:
+        readimagefile("images/Bot/Bot_Left3.gif", x1, y1, x2-1, y2-1);break;
+    case 4:
+        readimagefile("images/Bot/Bot_Left4.gif", x1, y1, x2-1, y2-1);break;
+    case 5:
+        readimagefile("images/Bot/Bot_Left5.gif", x1, y1, x2-1, y2-1);break;
+    case 6:
+        readimagefile("images/Bot/Bot_Left6.gif", x1, y1, x2-1, y2-1);break;
+    case 7:
+        readimagefile("images/Bot/Bot_Left7.gif", x1, y1, x2-1, y2-1);break;
+    }
+    (*urutan)++;
+    if(*urutan > 7){
+        *urutan = 0;
+    }
+
+}
+
+void drawBot(int x1,int y1,int x2, int y2){
+// menggambar bot
+    readimagefile("images/Bot/Bot_Left0.gif",x1, y1, x2-1, y2-1);
+}
+
+void insertBot(game arr[BARIS][KOLOM],int baris, int kolom){
+// memasukkan bot dalam matriks game
     arr[baris][kolom].bot=1;
 }
 
-void insertPlayer(game arr[BARIS][KOLOM],int baris, int kolom){ //memasukkan player dalam matriks game
+void insertPlayer(game arr[BARIS][KOLOM],int baris, int kolom){
+// memasukkan player dalam matriks game
     arr[baris][kolom].player=1;
 }
 
-void deleteBot(game arr[BARIS][KOLOM], int baris, int kolom){ //menghapus bot dalam matriks game
+void deleteBot(game arr[BARIS][KOLOM], int baris, int kolom){
+// menghapus bot dalam matriks game
     arr[baris][kolom].bot=0;
 }
 
-void deletePlayer(game arr[BARIS][KOLOM],int baris, int kolom){ //menghapus player dalam matriks game
+void deletePlayer(game arr[BARIS][KOLOM],int baris, int kolom){
+// menghapus player dalam matriks game
     arr[baris][kolom].player=0;
 }
 
-void level1(game arr[BARIS][KOLOM],int* barisPlayer, int* kolomPlayer){ //generate matriks game level 1
+void level1(game arr[BARIS][KOLOM],int* barisPlayer, int* kolomPlayer){
+// generate matriks game level 1
     for(int i=0;i<BARIS;i++){ //generate matriks komposit
         for(int j=0;j<KOLOM;j++){
             arr[i][j].player=0; //subvariabel player diisi dengan nilai 0 semua
@@ -62,6 +189,11 @@ void level1(game arr[BARIS][KOLOM],int* barisPlayer, int* kolomPlayer){ //genera
     arr[BARIS-9][7].stage = 1;
     arr[BARIS-9][6].stage = 1;
     arr[BARIS-9][5].stage = 1;
+    arr[BARIS-6][4].stage = 3;
+    arr[BARIS-6][5].stage = 3;
+    arr[BARIS-6][6].stage = 3;
+    arr[BARIS-6][7].stage = 3;
+    arr[BARIS-6][8].stage = 3;
     arr[BARIS-10][5].stage = 2;
     arr[BARIS-11][5].stage = 2;
     arr[BARIS-12][5].stage = 2;
@@ -92,14 +224,13 @@ void level1(game arr[BARIS][KOLOM],int* barisPlayer, int* kolomPlayer){ //genera
     arr[BARIS-18][15].stage = 5;
 
 
-
-
     *barisPlayer = BARIS-2; *kolomPlayer = KOLOM/2;
     insertPlayer(arr,*barisPlayer, *kolomPlayer);
     insertBot(arr,BARIS-2,KOLOM-2);
 }
 
-void generateStage(game arr[BARIS][KOLOM], int level, int* barisPlayer, int* kolomPlayer){ //
+void generateStage(game arr[BARIS][KOLOM], int level, int* barisPlayer, int* kolomPlayer){
+//Pemilihan level yang akan di generate
     switch(level){
     case 1 :
         level1(arr,barisPlayer,kolomPlayer);
@@ -107,7 +238,9 @@ void generateStage(game arr[BARIS][KOLOM], int level, int* barisPlayer, int* kol
     }
 }
 
-void drawStage(game arr[BARIS][KOLOM]){ // menggambar seluruh matriks game
+void drawStage(game arr[BARIS][KOLOM]){
+// menggambar seluruh matriks game
+
     for(int i=0; i<BARIS; i++){
         for(int j=0; j<KOLOM; j++){
             //penggambaran stage
@@ -125,7 +258,7 @@ void drawStage(game arr[BARIS][KOLOM]){ // menggambar seluruh matriks game
 
             //penggambaran player
             if(arr[i][j].player == 1){
-                drawPlayer(MATRIX_ELEMENT_SIZE*j,MATRIX_ELEMENT_SIZE*i,MATRIX_ELEMENT_SIZE*(j+1),MATRIX_ELEMENT_SIZE*(i+1));
+                drawPlayerRight(MATRIX_ELEMENT_SIZE*j,MATRIX_ELEMENT_SIZE*i,MATRIX_ELEMENT_SIZE*(j+1),MATRIX_ELEMENT_SIZE*(i+1));
             }
 
             //penggambaran bot
@@ -136,7 +269,18 @@ void drawStage(game arr[BARIS][KOLOM]){ // menggambar seluruh matriks game
     }
 }
 
-bool isNabrak(game arr[BARIS][KOLOM], int baris, int kolom, int arah){// arah = -1 kalau ke kiri dan arah = 1 kalau ke kanan
+bool isNabrak(game arr[BARIS][KOLOM], int X, int Y, int arah){
+// Pengecekan sisi kiri dan kanan player, jika ada tembok return true dan jika tidak ada return false
+// arah = -1 kalau ke kiri dan arah = 1 kalau ke kanan
+    int baris, kolom;
+
+    baris = (Y +(MATRIX_ELEMENT_SIZE/2))/MATRIX_ELEMENT_SIZE;
+    if(arah == 1){
+        kolom = X/MATRIX_ELEMENT_SIZE;
+    }else{
+        kolom = (X+(MATRIX_ELEMENT_SIZE-1))/MATRIX_ELEMENT_SIZE;
+    }
+
     if((arr[baris][kolom+arah].stage == 1)){
         return true;
     }else{
@@ -144,49 +288,68 @@ bool isNabrak(game arr[BARIS][KOLOM], int baris, int kolom, int arah){// arah = 
     }
 }
 
-void drawPlayerMovement(char movement, game arr[BARIS][KOLOM], int barisPlayer, int kolomPlayer){ //menggambar player setelah posisinya diubah
+void drawPlayerMovement(char movement, game arr[BARIS][KOLOM], int barisPlayer, int kolomPlayer, int X, int Y,int* urutan){
+//menggambar player setelah posisinya diubah
     switch(movement){
     case 'A' :
-            setviewport((kolomPlayer*MATRIX_ELEMENT_SIZE), (barisPlayer*MATRIX_ELEMENT_SIZE),((kolomPlayer+3)*MATRIX_ELEMENT_SIZE), ((barisPlayer+1)*MATRIX_ELEMENT_SIZE),1);
+            setviewport(((kolomPlayer-1)*MATRIX_ELEMENT_SIZE), (barisPlayer*MATRIX_ELEMENT_SIZE),((kolomPlayer+3)*MATRIX_ELEMENT_SIZE), ((barisPlayer+1)*MATRIX_ELEMENT_SIZE),1);
             clearviewport();
-            setviewport(((kolomPlayer+1)*MATRIX_ELEMENT_SIZE), ((barisPlayer-1)*MATRIX_ELEMENT_SIZE),((kolomPlayer+2)*MATRIX_ELEMENT_SIZE), ((barisPlayer+2)*MATRIX_ELEMENT_SIZE),1);
+            setviewport(((kolomPlayer)*MATRIX_ELEMENT_SIZE), ((barisPlayer-1)*MATRIX_ELEMENT_SIZE),((kolomPlayer+2)*MATRIX_ELEMENT_SIZE), ((barisPlayer+2)*MATRIX_ELEMENT_SIZE),1);
             clearviewport();
             setviewport(0,0, 800,600,1);
-            draw2right(arr,kolomPlayer,barisPlayer);
-            draw2down(arr,kolomPlayer+1,barisPlayer-1);
+            drawRight(arr,kolomPlayer-1,barisPlayer,4);
+            drawRight(arr,kolomPlayer,barisPlayer-1,2);
+            drawRight(arr,kolomPlayer,barisPlayer+1,2);
+            playerRunningLeft(X,Y,X+MATRIX_ELEMENT_SIZE,Y+MATRIX_ELEMENT_SIZE, urutan);
             break;
     case 'D' :
-            setviewport(((kolomPlayer-2)*MATRIX_ELEMENT_SIZE), (barisPlayer*MATRIX_ELEMENT_SIZE),((kolomPlayer+1)*MATRIX_ELEMENT_SIZE), ((barisPlayer+1)*MATRIX_ELEMENT_SIZE),1);
+            setviewport(((kolomPlayer-2)*MATRIX_ELEMENT_SIZE), (barisPlayer*MATRIX_ELEMENT_SIZE),((kolomPlayer+2)*MATRIX_ELEMENT_SIZE), ((barisPlayer+1)*MATRIX_ELEMENT_SIZE),1);
             clearviewport();
-            setviewport(((kolomPlayer-1)*MATRIX_ELEMENT_SIZE), ((barisPlayer-1)*MATRIX_ELEMENT_SIZE),((kolomPlayer)*MATRIX_ELEMENT_SIZE), ((barisPlayer+2)*MATRIX_ELEMENT_SIZE),1);
+            setviewport(((kolomPlayer-1)*MATRIX_ELEMENT_SIZE), ((barisPlayer-1)*MATRIX_ELEMENT_SIZE),((kolomPlayer+1)*MATRIX_ELEMENT_SIZE), ((barisPlayer+2)*MATRIX_ELEMENT_SIZE),1);
             clearviewport();
             setviewport(0,0, 800,600,1);
-            draw2left(arr,kolomPlayer,barisPlayer);
-            draw2down(arr,kolomPlayer-1,barisPlayer-1);
+            drawLeft(arr,kolomPlayer+1,barisPlayer,4);
+            drawLeft(arr,kolomPlayer,barisPlayer-1,2);
+            drawLeft(arr,kolomPlayer,barisPlayer+1,2);
+            playerRunningRight(X,Y,X+MATRIX_ELEMENT_SIZE,Y+MATRIX_ELEMENT_SIZE, urutan);
             break;
     case 'W' :
-            setviewport((kolomPlayer*MATRIX_ELEMENT_SIZE), (barisPlayer*MATRIX_ELEMENT_SIZE),((kolomPlayer+1)*MATRIX_ELEMENT_SIZE), ((barisPlayer+3)*MATRIX_ELEMENT_SIZE),1);
+            setviewport((kolomPlayer*MATRIX_ELEMENT_SIZE), ((barisPlayer)*MATRIX_ELEMENT_SIZE),((kolomPlayer+1)*MATRIX_ELEMENT_SIZE), ((barisPlayer+3)*MATRIX_ELEMENT_SIZE),1);
             clearviewport();
-            setviewport(((kolomPlayer-1)*MATRIX_ELEMENT_SIZE), ((barisPlayer+1)*MATRIX_ELEMENT_SIZE),((kolomPlayer+2)*MATRIX_ELEMENT_SIZE), ((barisPlayer+2)*MATRIX_ELEMENT_SIZE),1);
+            setviewport(((kolomPlayer-1)*MATRIX_ELEMENT_SIZE), ((barisPlayer)*MATRIX_ELEMENT_SIZE),((kolomPlayer+2)*MATRIX_ELEMENT_SIZE), ((barisPlayer+2)*MATRIX_ELEMENT_SIZE),1);
             clearviewport();
             setviewport(0,0, 800,600,1);
-            draw2down(arr,kolomPlayer,barisPlayer);
-            draw2right(arr,kolomPlayer-1,barisPlayer+1);
+            drawDown(arr,kolomPlayer,barisPlayer,3);
+            drawRight(arr,kolomPlayer-1,barisPlayer+1,3);
+            drawRight(arr,kolomPlayer-1,barisPlayer,3);
+            playerRunningRight(X,Y,X+MATRIX_ELEMENT_SIZE,Y+MATRIX_ELEMENT_SIZE, urutan);
             break;
     case 'S' :
-            setviewport((kolomPlayer*MATRIX_ELEMENT_SIZE), ((barisPlayer-2)*MATRIX_ELEMENT_SIZE),((kolomPlayer+1)*MATRIX_ELEMENT_SIZE), ((barisPlayer+1)*MATRIX_ELEMENT_SIZE),1);
+            setviewport((kolomPlayer*MATRIX_ELEMENT_SIZE), ((barisPlayer-2)*MATRIX_ELEMENT_SIZE),((kolomPlayer+1)*MATRIX_ELEMENT_SIZE), ((barisPlayer+2)*MATRIX_ELEMENT_SIZE),1);
             clearviewport();
-            setviewport(((kolomPlayer-1)*MATRIX_ELEMENT_SIZE), ((barisPlayer-1)*MATRIX_ELEMENT_SIZE),((kolomPlayer+2)*MATRIX_ELEMENT_SIZE), ((barisPlayer)*MATRIX_ELEMENT_SIZE),1);
+            setviewport(((kolomPlayer-1)*MATRIX_ELEMENT_SIZE), ((barisPlayer-1)*MATRIX_ELEMENT_SIZE),((kolomPlayer+2)*MATRIX_ELEMENT_SIZE), ((barisPlayer+1)*MATRIX_ELEMENT_SIZE),1);
             clearviewport();
             setviewport(0,0, 800,600,1);
-            draw2up(arr,kolomPlayer,barisPlayer);
-            draw2right(arr, kolomPlayer-1,barisPlayer-1);
+            drawUp(arr,kolomPlayer,barisPlayer+1,4);
+            drawRight(arr, kolomPlayer-1,barisPlayer-1,3);
+            drawRight(arr, kolomPlayer-1,barisPlayer,3);
+            playerRunningRight(X,Y,X+MATRIX_ELEMENT_SIZE,Y+MATRIX_ELEMENT_SIZE, urutan);
             break;
+    /*default:
+            setviewport(((kolomPlayer-1)*MATRIX_ELEMENT_SIZE), ((barisPlayer)*MATRIX_ELEMENT_SIZE),((kolomPlayer+2)*MATRIX_ELEMENT_SIZE), ((barisPlayer+1)*MATRIX_ELEMENT_SIZE),1);
+            clearviewport();
+            setviewport(((kolomPlayer)*MATRIX_ELEMENT_SIZE), ((barisPlayer-1)*MATRIX_ELEMENT_SIZE),((kolomPlayer+1)*MATRIX_ELEMENT_SIZE), ((barisPlayer+2)*MATRIX_ELEMENT_SIZE),1);
+            clearviewport();
+            setviewport(0,0, 800,600,1);
+            drawRight(arr, kolomPlayer-1,barisPlayer,3);
+            drawDown(arr, kolomPlayer,barisPlayer-1,3);*/
+
     }
 
 }
 
 void loading(){
+// menampilkan tampilan loading selagi matriks digambar
     setactivepage(3);
     settextstyle(10, 0, 8);
     outtextxy((WINDOWS_WIDTH/2)-250,(WINDOWS_HEIGHT/2)-50, "Lode Runner");
@@ -196,8 +359,9 @@ void loading(){
     setvisualpage(3);
 }
 
-bool isGerak(game arr[BARIS][KOLOM], int baris, int kolom, int barisBef, int kolomBef){
-    if((arr[baris][kolom].player == arr[barisBef][kolomBef].player)){
+bool isGerak(game arr[BARIS][KOLOM], int X, int Y, int XBfr, int YBfr){
+// mengecek apakah ada perubahan posisi player, jika ada return true, jika tidak return false
+    if((X == XBfr) && (Y == YBfr)){
         return false;
     }else{
         return true;
@@ -205,8 +369,8 @@ bool isGerak(game arr[BARIS][KOLOM], int baris, int kolom, int barisBef, int kol
 }
 
 void prosesInput(char* movement){
+//memproses input yang dimasukkan oleh user
     if ((*movement != 'A') && (*movement != 'S') && (*movement != 'D') && (*movement != 'W')){
         *movement = NULL;
     }
 }
-
