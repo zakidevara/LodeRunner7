79d7c3f19322b6409d0f689999fd092101fa5c54
agusnidhomm <48077840+agusnidhomm@users.noreply.github.com>
2019-03-28 11:58:26 +0700

    durasi lubang

--- 181511003.h
+++ 181511003.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <graphics.h>
 #include <conio.h>
+
+
 bool isStanding(game arr[BARIS][KOLOM], int baris, int kolom){
     if((arr[baris+1][kolom].stage == 1) || (arr[baris+1][kolom].stage == 2)){
         return true;
@@ -32,13 +34,14 @@ bool isFalling(game arr[BARIS][KOLOM], int baris, int kolom){
     }
 }
 
-void playerMovement(char movement, game arr[BARIS][KOLOM], int* barisPlayer, int* kolomPlayer, int *X, int* Y){ //memindahkan posisi player dalam matriks sesuai movement yang dipilih oleh user
-    switch(movement){
+void playerMovement(char movement, game arr[BARIS][KOLOM], int* barisPlayer, int* kolomPlayer, int *X, int* Y, clock_t* wktnembak, int*baristembak, int*kolomtembak){ //memindahkan posisi player dalam matriks sesuai movement yang dipilih oleh user
+    
+	switch(movement){
             case 'W' :
             if((*Y-5 >= 0) && (isClimbing(arr, *barisPlayer, *kolomPlayer)) || ((arr[*barisPlayer+1][*kolomPlayer].stage == 2))){
                 *X = (*kolomPlayer)*MATRIX_ELEMENT_SIZE;
                 *Y= *Y - 5;
-            }
+			}         
             break;
 
             case 'S' :
@@ -59,7 +62,6 @@ void playerMovement(char movement, game arr[BARIS][KOLOM], int* barisPlayer, int
                 if(isStanding(arr, *barisPlayer, *kolomPlayer) && !isClimbing(arr, *barisPlayer, *kolomPlayer)){
                     *Y = (*barisPlayer)*MATRIX_ELEMENT_SIZE;
                 }
-
                 *X = *X + 5;
             }
             break;
@@ -73,11 +75,19 @@ void playerMovement(char movement, game arr[BARIS][KOLOM], int* barisPlayer, int
             break;
             case 'M' :
             	if(arr[*barisPlayer+1][*kolomPlayer+1].stage == 1){
-            		arr[*barisPlayer+1][*kolomPlayer+1].stage = 0;
-				}break;
+            		*baristembak = *barisPlayer+1;
+            		*kolomtembak = *kolomPlayer+1;
+					arr[*barisPlayer+1][*kolomPlayer+1].stage = 0;
+            		*wktnembak = clock();
+            	}
+				break;
 			case 'N' :
 				if(arr[*barisPlayer+1][*kolomPlayer-1].stage == 1){
+					*baristembak = *barisPlayer-1;
+            		*kolomtembak = *kolomPlayer-1;
             		arr[*barisPlayer+1][*kolomPlayer-1].stage = 0;
-				}break;
+            		*wktnembak = clock();
+				} 
+				break;
         }
 }
