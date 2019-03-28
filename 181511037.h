0b6afebe233cacde5ad90b487110956cb59bdfc6
an1sal <48077693+an1sal@users.noreply.github.com>
2019-03-28 14:52:54 +0700

    Update modul menu 181511037.h

--- 181511037.h
+++ 181511037.h
@@ -101,46 +101,68 @@ void drawLeft(game arr[BARIS][KOLOM], int kolom, int baris, int n){ //menggambar
 
 void menutama()
 {
-    initwindow(getmaxwidth(), getmaxheight(), " ", 0, 0,false,true);
-    int playy = (600/2)+100;
-    int credity = playy+70;
-    int exity = credity+70;
+    initwindow(800, 650, " ", 0, 0, false, true);
+
     int mousex,mousey;
     char pil;
 
 
     while(!ismouseclick(WM_LBUTTONDOWN)){
-
-    settextstyle(BOLD_FONT,HORIZ_DIR,7);
-    outtextxy(800/2,600/2,"LODE RUNNER");
-    settextstyle(EUROPEAN_FONT,HORIZ_DIR,4);
-    outtextxy(1100/2,playy,"PLAY");
-    settextstyle(EUROPEAN_FONT,HORIZ_DIR,4);
-    outtextxy(1050/2,credity,"CREDIT");
-    settextstyle(EUROPEAN_FONT,HORIZ_DIR,4);
-    outtextxy(1100/2,exity,"EXIT");
+    settextstyle(SCRIPT_FONT,HORIZ_DIR,3);
+    outtextxy(10,10,"click to choose!!");
+    settextstyle(SANS_SERIF_FONT,HORIZ_DIR,6);
+    outtextxy(210,200,"LODE RUNNER");
+    // play
+    settextstyle(COMPLEX_FONT,HORIZ_DIR,5);
+    outtextxy(220,280,"P");
+    outtextxy(220,320,"L");
+    outtextxy(220,360,"A");
+    outtextxy(220,400,"Y");
+    //high score
+    outtextxy(270,280,"H");
+    outtextxy(270,320,"I");
+    outtextxy(270,360," ");
+    outtextxy(270,400,"S");
+    outtextxy(270,440,"C");
+    outtextxy(270,480,"O");
+    outtextxy(270,520,"R");
+    outtextxy(270,560,"E");
+    // exit
+    outtextxy(565,300,"E");
+    outtextxy(565,340,"X");
+    outtextxy(565,380,"I");
+    outtextxy(565,420,"T");
 
 
     }
     while(1){
     getmouseclick(WM_LBUTTONDOWN, mousex, mousey);
     //menu play
-    if ((mousex > 540) && (mousex < 675)&& (mousey > 390) && (mousey < 440)){
-        exit(1);
+    if ((mousex > 217) && (mousex < 248)&& (mousey > 298) && (mousey < 463)){
+        setactivepage(0);
+        cleardevice();
+        setactivepage(1);
+        cleardevice();
+        permainan();
+        delay(1000);
+        getch();
+        closegraph(-1);
+        break;
     //menu credit
-        }else if ((mousex > 515) && (mousex < 695)&& (mousey > 460) && (mousey < 510)){
-            exit(1);
+        //}else if ((mousex > 515) && (mousex < 695)&& (mousey > 460) && (mousey < 510)){
+          //  exit(1);
     //menu exit
-                }else if ((mousex > 540) && (mousex < 659)&& (mousey > 530) && (mousey < 580)){
+                }else if ((mousex > 562) && (mousex < 594)&& (mousey > 298) && (mousey < 463)){
                     cleardevice();
-                    settextstyle(COMPLEX_FONT,HORIZ_DIR,10);
-                    outtextxy(350,300,"GOOD BYE");
+                    settextstyle(SANS_SERIF_FONT,HORIZ_DIR,6);
+                    outtextxy(250,200,"Good Bye");
                     delay(1000);
                     exit(1);
                 }
     }
-    printf("%d %d", mousex, mousey);
+    printf("mouse: %d, mousey: %d", mousex, mousey);
     getch();
+
 }
 
 
