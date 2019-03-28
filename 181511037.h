f9bf053d440f2a61132096155b96e461ed9beb90
zakidevara <mzakidevara@gmail.com>
2019-03-28 07:52:08 +0700

    ubah nama modul

--- 181511037.h
+++ 181511037.h
@@ -3,9 +3,11 @@
 #include <conio.h>
 
 
-void draw2down(game arr[BARIS][KOLOM], int kolom, int baris){ //menggambar matriks sebanyak 2 block ke bawah.
 
-    for(int i=0;i<3;i++){
+
+void drawDown(game arr[BARIS][KOLOM], int kolom, int baris, int n){ //menggambar matriks sebanyak 2 block ke bawah.
+
+    for(int i=0;i<n;i++){
         //penggambaran stage
         if(arr[baris+i][kolom].stage == 1){
             drawBlock(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris+i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1+i));
@@ -19,20 +21,16 @@ void draw2down(game arr[BARIS][KOLOM], int kolom, int baris){ //menggambar matri
             drawExit(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris+i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1+i));
         }
 
-        //penggambaran player
-        if(arr[baris+i][kolom].player == 1){
-            drawPlayer(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris+i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1+i));
+        if(arr[baris+i][kolom].bot == 1){
+                drawBot(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris+i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1+i));
         }
 
-        //penggambaran bot
-        if(arr[baris+i][kolom].bot == 1){
-            drawBot(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris+i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1+i));
-        }
+
     }
 }
 
-void draw2up(game arr[BARIS][KOLOM], int kolom, int baris){ //menggambar matriks sebanyak 2 block ke bawah.
-    for(int i=0;i<3;i++){
+void drawUp(game arr[BARIS][KOLOM], int kolom, int baris, int n){ //menggambar matriks sebanyak 2 block ke bawah.
+    for(int i=0;i<n;i++){
         //penggambaran stage
         if(arr[baris-i][kolom].stage == 1){
             drawBlock(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris-i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1-i));
@@ -46,20 +44,16 @@ void draw2up(game arr[BARIS][KOLOM], int kolom, int baris){ //menggambar matriks
             drawExit(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris-i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1-i));
         }
 
-        //penggambaran player
-        if(arr[baris-i][kolom].player == 1){
-            drawPlayer(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris-i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1-i));
+        if(arr[baris-i][kolom].bot == 1){
+                drawBot(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris-i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1-i));
         }
 
-        //penggambaran bot
-        if(arr[baris-i][kolom].bot == 1){
-            drawBot(MATRIX_ELEMENT_SIZE*kolom, MATRIX_ELEMENT_SIZE*(baris-i), MATRIX_ELEMENT_SIZE*(kolom+1), MATRIX_ELEMENT_SIZE*(baris+1-i));
-        }
+
     }
 }
 
-void draw2right(game arr[BARIS][KOLOM], int kolom, int baris){ //menggambar matriks sebanyak 2 block ke bawah.
-    for(int i=0;i<3;i++){
+void drawRight(game arr[BARIS][KOLOM], int kolom, int baris, int n){ //menggambar matriks sebanyak 2 block ke bawah.
+    for(int i=0;i<n;i++){
         //penggambaran stage
         if(arr[baris][kolom+i].stage == 1){
             drawBlock(MATRIX_ELEMENT_SIZE*(kolom+i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom+i+1), MATRIX_ELEMENT_SIZE*(baris+1));
@@ -73,20 +67,16 @@ void draw2right(game arr[BARIS][KOLOM], int kolom, int baris){ //menggambar matr
             drawExit(MATRIX_ELEMENT_SIZE*(kolom+i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom+i+1), MATRIX_ELEMENT_SIZE*(baris+1));
         }
 
-        //penggambaran player
-        if(arr[baris][kolom+i].player == 1){
-            drawPlayer(MATRIX_ELEMENT_SIZE*(kolom+i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom+i+1), MATRIX_ELEMENT_SIZE*(baris+1));
+        if(arr[baris][kolom+i].bot == 1){
+                drawBot(MATRIX_ELEMENT_SIZE*(kolom+i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom+i+1), MATRIX_ELEMENT_SIZE*(baris+1));
         }
 
-        //penggambaran bot
-        if(arr[baris][kolom+i].bot == 1){
-            drawBot(MATRIX_ELEMENT_SIZE*(kolom+i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom+i+1), MATRIX_ELEMENT_SIZE*(baris+1));
-        }
+
     }
 }
 
-void draw2left(game arr[BARIS][KOLOM], int kolom, int baris){ //menggambar matriks sebanyak 2 block ke bawah.
-    for(int i=0;i<3;i++){
+void drawLeft(game arr[BARIS][KOLOM], int kolom, int baris, int n){ //menggambar matriks sebanyak 2 block ke bawah.
+    for(int i=0;i<n;i++){
         //penggambaran stage
         if(arr[baris][kolom-i].stage == 1){
             drawBlock(MATRIX_ELEMENT_SIZE*(kolom-i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom-i+1), MATRIX_ELEMENT_SIZE*(baris+1));
@@ -100,15 +90,11 @@ void draw2left(game arr[BARIS][KOLOM], int kolom, int baris){ //menggambar matri
             drawExit(MATRIX_ELEMENT_SIZE*(kolom-i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom-i+1), MATRIX_ELEMENT_SIZE*(baris+1));
         }
 
-        //penggambaran player
-        if(arr[baris][kolom-i].player == 1){
-            drawPlayer(MATRIX_ELEMENT_SIZE*(kolom-i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom-i+1), MATRIX_ELEMENT_SIZE*(baris+1));
+        if(arr[baris][kolom-i].bot == 1){
+                drawBot(MATRIX_ELEMENT_SIZE*(kolom-i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom-i+1), MATRIX_ELEMENT_SIZE*(baris+1));
         }
 
-        //penggambaran bot
-        if(arr[baris][kolom-i].bot == 1){
-            drawBot(MATRIX_ELEMENT_SIZE*(kolom-i), MATRIX_ELEMENT_SIZE*(baris), MATRIX_ELEMENT_SIZE*(kolom-i+1), MATRIX_ELEMENT_SIZE*(baris+1));
-        }
+
     }
 }
 
