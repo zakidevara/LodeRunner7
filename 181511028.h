42dc2afe5c9d6afcd1f59734b3b3077e5f3bfbb7
zakidevara <mzakidevara@gmail.com>
2019-03-29 07:46:48 +0700

    update tekstrur map

--- 181511028.h
+++ 181511028.h
@@ -4,11 +4,11 @@
 
 // DEKLARASI MODUL
 void drawRope(int x1,int y1,int x2, int y2){ //menggambar tangga
-    readimagefile("images/slide.gif",x1, y1, x2-1, y2-1);
+    readimagefile("images/rope.gif",x1, y1, x2-1, y2-1);
 }
 
 void drawCoin(int x1,int y1,int x2, int y2){ //menggambar tangga
-    readimagefile("images/goal.gif",x1, y1, x2-1, y2-1);
+    readimagefile("images/coin.gif",x1, y1, x2-1, y2-1);
 }
 
 void drawExit(int x1,int y1,int x2, int y2){ //menggambar tangga
@@ -22,7 +22,5 @@ void drawLadder(int x1,int y1,int x2, int y2){ //menggambar tangga
 
 
 void drawBlock(int x1,int y1,int x2, int y2){ //menggambar block
-    readimagefile("images/wall.gif",x1, y1, x2-1, y2-1);
+    readimagefile("images/bata.gif",x1, y1, x2-1, y2-1);
 }
-
-
