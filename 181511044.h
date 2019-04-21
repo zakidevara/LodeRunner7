908a3834742f24cfb0a87b3711aecc47a5418df8
fuadpuadun <48077664+fuadpuadun@users.noreply.github.com>
2019-04-21 14:49:55 +0700

    Update 181511044.h
    Masukin Sistem Skor

--- 181511044.h
+++ 181511044.h
@@ -17,6 +17,10 @@ double hitung_Waktu(clock_t wktAwal, clock_t wktAkhir);
 
 void tampil_Waktu(double durasi);
 
+int hitung_skor_akhir(int* skor, double durasi);
+
+void tampil_skor_akhir(int skor);
+
 bool lagiNgambilKoin(int arr[BARIS][KOLOM], int baris, int kolom );
 
 #endif // 1815110044_H
