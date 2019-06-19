e84dccd819f52a6f6b3eadc10330bdb4089e7b57
zakidevara <mzakidevara@gmail.com>
2019-06-20 01:25:43 +0700

    Perubahan struktur data bot jadi single linked list

---
+++ 181511003.cpp
@@ -0,0 +1,147 @@
+#include "181511003.h"
+
+bool isStanding(int arr[BARIS][KOLOM], int x, int y){
+    int baris = (y + MATRIX_ELEMENT_SIZE)/MATRIX_ELEMENT_SIZE;
+    int kolom = (x + MATRIX_ELEMENT_SIZE/2)/MATRIX_ELEMENT_SIZE;
+    if((arr[baris][kolom] == 1) || (arr[baris][kolom] == 2) || (arr[baris][kolom] == 6) || (arr[baris][kolom] == 9)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool isClimbing(int arr[BARIS][KOLOM], int x, int y){
+    int baris = (y )/MATRIX_ELEMENT_SIZE;
+    int baris2 = (y + MATRIX_ELEMENT_SIZE)/MATRIX_ELEMENT_SIZE;
+    int kolom = (x + MATRIX_ELEMENT_SIZE/2)/MATRIX_ELEMENT_SIZE;
+    if(((arr[baris][kolom] == 2) || (arr[baris2][kolom] == 2))) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool isSliding(int arr[BARIS][KOLOM], int x, int y){
+    int baris2 = y/MATRIX_ELEMENT_SIZE;
+    int baris = (y + MATRIX_ELEMENT_SIZE-1)/MATRIX_ELEMENT_SIZE;
+    int kolom = (x + MATRIX_ELEMENT_SIZE/2)/MATRIX_ELEMENT_SIZE;
+    if(arr[baris][kolom] == 3 && arr[baris2][kolom] == 3){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool isFalling(int arr[BARIS][KOLOM], int x, int y){
+    int baris2 = y/MATRIX_ELEMENT_SIZE;
+    int baris = (y + MATRIX_ELEMENT_SIZE)/MATRIX_ELEMENT_SIZE;
+    int kolom = (x + MATRIX_ELEMENT_SIZE/2)/MATRIX_ELEMENT_SIZE;
+    if(((arr[baris][kolom] == 0) || (arr[baris][kolom] == 3) || (arr[baris][kolom] == 7) || (arr[baris][kolom] == 4) || (arr[baris][kolom] == 8)) && (arr[baris2][kolom] != 2)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+void playerMovement(int arr[BARIS][KOLOM], QueueLubang* P, spriteInfo* player, int speed){ //memindahkan posisi player dalam matriks sesuai movement yang dipilih oleh user
+    lubang Z;
+	switch((*player).movement){
+            case KEY_UP :
+            case 'W' :
+            (*player).urutanBom=-1;
+            if(((*player).koor.Y - speed >= 0) && ((isClimbing(arr, player->koor.X, player->koor.Y)  && !isFalling(arr, player->koor.X, player->koor.Y - speed) ) || (arr[player->pm.baris][player->pm.kolom] == 1))){
+                (*player).koor.X = ((*player).pm.kolom)*MATRIX_ELEMENT_SIZE;
+                (*player).koor.Y = (*player).koor.Y - speed;
+			}else{
+                (*player).movement = NULL;
+			}
+            break;
+            case KEY_DOWN :
+            case 'S' :
+            (*player).urutanBom=-1;
+            if(((*player).koor.Y + speed < GAME_HEIGHT)  && ((arr[player->pm.baris+1][player->pm.kolom] == 2) ||(isClimbing(arr, player->koor.X, player->koor.Y) && !isStanding(arr, player->koor.X, player->koor.Y)) ||
+            (isSliding(arr, player->koor.X, player->koor.Y) && !isStanding(arr, player->koor.X, player->koor.Y)) || isFalling(arr, player->koor.X, player->koor.Y))){
+                if(isSliding(arr,(*player).pm.baris,(*player).pm.kolom)){
+                    (*player).koor.Y = (*player).koor.Y + MATRIX_ELEMENT_SIZE;
+                }else{
+                    (*player).koor.X = ((*player).pm.kolom)*MATRIX_ELEMENT_SIZE;
+                    (*player).koor.Y = (*player).koor.Y + speed;
+                }
+            }else{
+                (*player).movement = NULL;
+			}
+            break;
+            case KEY_RIGHT :
+            case 'D' :
+            (*player).urutanBom=-1;
+            if(((*player).koor.X < GAME_WIDTH-MATRIX_ELEMENT_SIZE) && !isNabrak(arr, (*player).koor.X, (*player).koor.Y, 1) && ((isStanding(arr, player->koor.X, player->koor.Y)==true)||
+                (isSliding(arr, player->koor.X, player->koor.Y)==true) || isClimbing(arr, player->koor.X, player->koor.Y))){
+                if((isStanding(arr, player->koor.X, player->koor.Y) && !isClimbing(arr, player->koor.X, player->koor.Y)) || isSliding(arr, player->koor.X, player->koor.Y)){
+                    (*player).koor.Y = ((*player).pm.baris)*MATRIX_ELEMENT_SIZE;
+                }
+                (*player).koor.X = (*player).koor.X + speed;
+            }else{
+                (*player).movement = NULL;
+			}
+            break;
+            case KEY_LEFT :
+            case 'A' :
+            (*player).urutanBom=-1;
+            if(((*player).koor.X > 0) && !isNabrak(arr, (*player).koor.X, (*player).koor.Y, -1) && ((isStanding(arr, player->koor.X, player->koor.Y)==true)||(isSliding(arr, player->koor.X, player->koor.Y)==true) ||
+                isClimbing(arr, player->koor.X, player->koor.Y))){
+                if((isStanding(arr, player->koor.X, player->koor.Y) && !isClimbing(arr, player->koor.X, player->koor.Y)) || isSliding(arr, player->koor.X, player->koor.Y)){
+                    (*player).koor.Y = ((*player).pm.baris)*MATRIX_ELEMENT_SIZE;
+                }
+                (*player).koor.X = (*player).koor.X - speed;
+            }else{
+                (*player).movement = NULL;
+			}
+            break;
+            case 'X' :
+            //case 'M' :
+            	if(((arr[(*player).pm.baris+1][(*player).pm.kolom+1] == 1)||(arr[(*player).pm.baris+1][(*player).pm.kolom+1] == 7)) && ((arr[(*player).pm.baris][(*player).pm.kolom+1] == 0) || (arr[(*player).pm.baris][(*player).pm.kolom+1] == 8))){
+            		((*player).urutanBom)++;
+            		player->urutanAnimasi = 0;
+
+            		if((*player).urutanBom > -1){
+                        arr[(*player).pm.baris+1][(*player).pm.kolom+1] = 7;
+                        if((*player).urutanBom == 4){
+                            arr[(*player).pm.baris+1][(*player).pm.kolom+1] = 8;
+                            Z = assign_Lubang((*player).pm.baris+1, (*player).pm.kolom+1, clock());
+                            enqueue(P, Z);
+                        }
+                    }
+            	}else{
+                    (*player).movement = NULL;
+                }
+				break;
+            case 'Z' :
+			case 'N' :
+				if(((arr[(*player).pm.baris+1][(*player).pm.kolom-1] == 1)||(arr[(*player).pm.baris+1][(*player).pm.kolom-1]== 7)) && ((arr[(*player).pm.baris][(*player).pm.kolom-1] == 0) || (arr[(*player).pm.baris][(*player).pm.kolom-1] == 8))){
+                    ((*player).urutanBom)++;
+                    player->urutanAnimasi = 8;
+
+                    if((*player).urutanBom>-1){
+                        arr[(*player).pm.baris+1][(*player).pm.kolom-1] = 7;
+                        if((*player).urutanBom == 4){
+                            arr[(*player).pm.baris+1][(*player).pm.kolom-1] = 8;
+                            Z = assign_Lubang((*player).pm.baris +1, (*player).pm.kolom-1, clock());
+                            enqueue(P, Z);
+                        }
+                    }
+
+				}else{
+                    (*player).movement = NULL;
+                }
+				break;
+            case FALL :
+                (*player).urutanBom=-1;
+                if((*player).koor.Y < GAME_HEIGHT){
+                    (*player).koor.X = player->pm.kolom * MATRIX_ELEMENT_SIZE;
+                    (*player).koor.Y = (*player).koor.Y + speed;
+                }else{
+                    (*player).movement = NULL;
+                }
+                break;
+        }
+}
---
+++ main.h
@@ -0,0 +1,105 @@
+#ifndef main_H
+#define main_H
+
+#include <stdio.h>
+#include <graphics.h>
+#include <conio.h>
+#include <ctime>
+#include <windows.h>
+#include <mmsystem.h>
+
+#define MATRIX_ELEMENT_SIZE 40
+#define BARIS 17
+#define KOLOM 28
+
+#define SCOREBAR 50
+#define GAME_HEIGHT BARIS*MATRIX_ELEMENT_SIZE
+#define GAME_WIDTH KOLOM*MATRIX_ELEMENT_SIZE
+
+#define WINDOWS_HEIGHT GAME_HEIGHT+SCOREBAR
+#define WINDOWS_WIDTH  GAME_WIDTH
+
+#define MAX_LEVEL 5
+#define FALL 1
+
+/* ---------------------------- Struktur Data Posisi ---------------------------- */
+typedef struct{
+    int X;
+    int Y;
+}koordinat;  //koordinat/posisi sprite dalam pixel
+
+typedef struct{
+    int baris;
+    int kolom;
+}posisiMatriks; //posisi sprite dalam matriks
+
+
+/* ----- List PosMatriks ----- */
+struct tElmtListPosMatriks{
+    posisiMatriks info;
+    tElmtListPosMatriks* next;
+};
+
+typedef struct{
+    tElmtListPosMatriks* head = NULL;
+}ListPosMatriks;
+
+
+/* ---------------------------- Struktur Data Level ---------------------------- */
+typedef struct{
+    int lv;
+    int arr[BARIS][KOLOM];
+    posisiMatriks exitPos;
+    posisiMatriks playerPos;
+    ListPosMatriks botPos;
+    int jmlBot;
+}infoLevel;            //info satu level
+
+/* ---------------------------- Struktur Data Sprite ---------------------------- */
+typedef struct{
+    koordinat koor;
+    posisiMatriks pm;
+    char movement = NULL;
+    int urutanAnimasi = 1;
+    int urutanBom = -1;
+    int lives = 3;
+}spriteInfo;                //struktur data suatu sprite
+
+
+
+typedef struct{
+    void* brick;
+    void* bedrock;
+    void* ladder;
+    void* coin;
+    void* exit;
+    void* rope;
+}blockSprite;  // Sprite block
+
+typedef struct{
+    void* running[16];
+    void* climbingLadder[2];
+    void* climbingRope[4];
+    void* bombing[2];
+}spriteAnim;
+
+
+/* ---------------------------- Struktur Data User ---------------------------- */
+typedef struct{
+    char nama[50];
+    int score = 0;
+}tUser;
+
+
+
+
+
+
+
+void tampilan_exit(double wkttotal, int score);
+void permainan();
+
+#endif // main_H
+
+
+
